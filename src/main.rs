9e7ce6c0093984756f81fb42ab1b7ca79a84f8ca
NicolasPllr1 <nicolas.pllr1@gmail.com>
2025-09-24 20:46:09 +0200

    feat: get cells count of sqlite_schema table
    In this challenge, we assume all rows in this table corresponds to
    tables, and they fit in a single page. Under these assumptions, this
    cell count is equal to the number of tables in the db!

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,23 @@ fn main() -> Result<()> {
     match command.as_str() {
         ".dbinfo" => {
             let mut file = File::open(&args[1])?;
-            let mut header = [0; 100];
-            file.read_exact(&mut header)?;
+            let mut db_header = [0; 100];
+            file.read_exact(&mut db_header)?;
 
             // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
             #[allow(unused_variables)]
-            let page_size = u16::from_be_bytes([header[16], header[17]]);
+            let page_size = u16::from_be_bytes([db_header[16], db_header[17]]);
 
             println!("database page size: {}", page_size);
+
+            // 'The two-byte integer at offset 3 gives the number of cells on the page.'
+            // from https://www.sqlite.org/fileformat.html#b_tree_pages,
+            // in the ' B-tree Page Header Format' table
+            let mut sqlite_schema_table_header = [0; 100];
+            file.read_exact(&mut sqlite_schema_table_header)?;
+            let nb_pages =
+                u16::from_be_bytes([sqlite_schema_table_header[3], sqlite_schema_table_header[4]]);
+            println!("number of tables: {}", nb_pages);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
